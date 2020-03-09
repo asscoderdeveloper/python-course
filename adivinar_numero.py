2a5383698b2485e3fdd2d3d1a7aaeaa5c39cf92c
Asscoder <asscoderdeveloper@gmail.com>
2020-03-09 23:22:58 +0100

    Acabado

--- adivinar_numero.py
+++ adivinar_numero.py
@@ -1,11 +1,14 @@
 import random
-numero_correcto = random.randint(1, 10)
-numero_participante = int(input("Elige un numero del 1 al 10"))
+numero_correcto = random.randint (1, 10)
+numero_participante = None
 while numero_participante != numero_correcto:
-    numero_participante = int(input("Elige otro número"))
+    numero_participante = int(input("Elige un numero del 1 al 10"))
     if numero_participante > 10:
         print("Te has pasado un poco...")
-    if numero_participante < 1:
+    elif numero_participante < 1:
         print("Te has quedado corto")
-if numero_participante == numero_correcto:
-    print("Has acertado, el numero correcto era: {}".format(numero_correcto))
+    if (numero_correcto -1) == numero_participante:
+        print("Ui... casi")
+    if   numero_participante == (numero_correcto +1):
+        print("Ui casi...")
+print("Has acertado, el numero correcto era: {}".format(numero_correcto))
