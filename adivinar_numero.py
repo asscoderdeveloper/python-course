7b6324ddbad04785ba559997ece2a3f8373217dd
Asscoder <asscoderdeveloper@gmail.com>
2020-03-09 18:15:59 +0100

    He puesto un while para que sea mas eficaz

--- adivinar_numero.py
+++ adivinar_numero.py
@@ -1,12 +1,11 @@
 import random
 numero_correcto = random.randint(1, 10)
 numero_participante = int(input("Elige un numero del 1 al 10"))
-if numero_participante > 10:
-    print("Te has pasado un poco...")
-if numero_participante < 1:
-    print("Te has quedado corto")
-if numero_participante != numero_correcto:
-    print("Buen intento,hasta la próxima")
-    print("El numero ganador era: {}".format(numero_correcto))
+while numero_participante != numero_correcto:
+    numero_participante = int(input("Elige otro número"))
+    if numero_participante > 10:
+        print("Te has pasado un poco...")
+    if numero_participante < 1:
+        print("Te has quedado corto")
 if numero_participante == numero_correcto:
     print("Has acertado, el numero correcto era: {}".format(numero_correcto))
